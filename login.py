cc41cb939a5cd88f8c4bd0dd725b030b0148060a
Wenyong-Y <498651225@qq.com>
2016-11-17 01:48:57 +0100

    Update login.py

--- login.py
+++ login.py
@@ -1,26 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
-import urllib
-import urllib2
+# import urllib
+# import urllib2
 # import re
 import cookielib
 import requests
 
 
 class Login(object):
-
+    # the login site and the post login site (home pqge)
     login_url = "AZE"
     home_url = "httAZE"
 
+    # path of the cookies file
     path = os.path.dirname(os.path.abspath(__file__))
     filePath = path + 'cookie.txt'
 
+    # headers setting
     headers = {
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-        #"Accept-Encoding": "gzip, deflate, sdch, br",
+        # "Accept-Encoding": "gzip, deflate, sdch, br",
         "Accept-Language": "en-US,en;q=0.8",
-        #"Cache-Control": "max-age=0",
+        # "Cache-Control": "max-age=0",
         "Connection": "keep-Alive",
         "Content-Type": "application/x-www-form-urlencoded",
         "Host": "mib",
@@ -30,46 +32,44 @@ class Login(object):
         "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.101 Safari/537.36"
     }
 
-    def __init__(self, session, mode):
+    def __init__(self, mode):
         """
         mode 0: log in with credentials
         mode 1: session recovery by cookies
         """
         self.mode = mode
-        self.session = session
 
     def sessionBuild(self):
+        """
+        Build up and return a session that automatically deal with the cookies
+        """
         # disable the urllib3 warnings when using old version python
         requests.packages.urllib3.disable_warnings()
-        # 建立一个会话，可以把同一用户的不同请求联系起来；直到会话结束都会自动处理cookies
+        # set up session that can automatically deal with the cookies
         session = requests.Session()
-        # 建立LWPCookieJar实例，可以存Set-Cookie3类型的文件。
+        # set up an object to stock the cookies
         session.cookies = cookielib.MozillaCookieJar(Login.filePath)
         return session
 
-    def loginWithCredentials(self):
+    def loginByAuthentication(self):
         """
         Enter your username and password to log in
         """
-        # headers setting
-
-        # credentials requirement
+        # username + password
         username = raw_input("Please enter your login :\n")
-        password = raw_input("Please enter your password :\n")
+        password = raw_input("Then please enter your password :\n")
         # data to send to the site in case of login
         dataAndCredentials = {
             "login": username,
             "foolAutofillFromBrowser": "",
             "password": password,
             "sub_auth": "Login"
-            # '_csrf': Getcsrf(),
         }
-
+        # Start to log on
         try:
-            # Start to log on
-            print 'Loging on to , Please wait...'
-            # Send the request to the site to log in
-            response = session.post(Login.login_url, data=data, headers=Login.headers, verify=False)  # allow_redirects=True,
+            print 'Loging on to XXX Please wait...'
+            # Send the request to the site to log on
+            response = session.post(Login.login_url, data=dataAndCredentials, headers=Login.headers, verify=False)
             # Print the HTML source of the page after login
             print response.text
 
@@ -97,8 +97,10 @@ class Login(object):
         except Exception as e:
             raise
 
-        # save cookies to local storage to maintain the seesion (connection)
-        session.cookies.save(ignore_discard=True, ignore_expires=True)
+        else:
+            # save cookies to local storage to maintain the seesion
+            # (connection)
+            session.cookies.save(ignore_discard=True, ignore_expires=True)
 
         def SessionMaintain(self, home_url):
             """
@@ -109,7 +111,8 @@ class Login(object):
             try:
                 # Try to load the cookies stocked in a local file
                 print 'Loading cookies...'
-                session.cookies.load(filename=Login.filePath, ignore_discard=True, ignore_expires=True)
+                session.cookies.load(
+                    filename=Login.filePath, ignore_discard=True, ignore_expires=True)
                 print 'Cookie loaded with success, waiting for the response'
                 # When cookies loaded, go to the site after login
                 response = session.get(Login.home_url, verify=False)
@@ -141,9 +144,10 @@ class Login(object):
 
             except Exception as e:
                 raise
-
-            # save cookies to local storage to maintain the seesion (connection)
-            session.cookies.save(ignore_discard=True, ignore_expires=True)
+            else:
+                # save cookies to local storage to maintain the seesion
+                # (connection)
+                session.cookies.save(ignore_discard=True, ignore_expires=True)
 
         def Getcsrf(self):
             """
@@ -152,15 +156,27 @@ class Login(object):
             response = session.get(self.login_url, headers=Login.headers)
             html = response.text
             print html
-            get_csrf_pattern = re.compile(r'< the match pattern for the regular expression search >')
+            get_csrf_pattern = re.compile(
+                r'< the match pattern for the regular expression search >')
             '''
                find all the information corresponding to the pattern, and return a list.
                Normally, there'll be just one element in the list. So take the first element
             '''
             _csrf = re.findall(get_csrf_pattern, html)[0]
-            # return the element extracted previously
+            # returloginn the element extracted previously
             return _csrf
 
+
+class Extract(object):
+
+    def __init__(self):
+        pass
+
+
+class WriteExcel(object):
+
+    def __init__(self):
+        pass
 
 # print os.path.dirname(os.path.abspath(__file__))
 # print os.getcwd()
