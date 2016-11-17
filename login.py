dd01da6137c88e539a114c1e647682aacdd12520
Wenyong-Y <498651225@qq.com>
2016-11-17 18:29:15 +0100

    Update login.py

--- login.py
+++ login.py
@@ -1,21 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import os
 # import urllib
 # import urllib2
 # import re
+import os
 import cookielib
 import requests
 
 
-class Login(object):
-    # the login site and the post login site (home pqge)
-    login_url = "AZE"
-    home_url = "httAZE"
+class Login:
 
     # path of the cookies file
     path = os.path.dirname(os.path.abspath(__file__))
-    filePath = path + 'cookie.txt'
+    filePath = path + '\session_cookie.txt'
 
     # headers setting
     headers = {
@@ -32,12 +29,15 @@ class Login(object):
         "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.101 Safari/537.36"
     }
 
-    def __init__(self, mode):
+    def __init__(self, login_url, home_url):
+        # the login site and the post login site (home pqge)
+        self.login_url = login_url
+        self.home_url = home_url
         """
         mode 0: log in with credentials
         mode 1: session recovery by cookies
         """
-        self.mode = mode
+        # self.mode = mode
 
     def sessionBuild(self):
         """
@@ -51,7 +51,7 @@ class Login(object):
         session.cookies = cookielib.MozillaCookieJar(Login.filePath)
         return session
 
-    def loginByAuthentication(self):
+    def loginByAuthentication(self, session):
         """
         Enter your username and password to log in
         """
@@ -69,7 +69,7 @@ class Login(object):
         try:
             print 'Loging on to XXX Please wait...'
             # Send the request to the site to log on
-            response = session.post(Login.login_url, data=dataAndCredentials, headers=Login.headers, verify=False)
+            response = session.post(self.login_url, data=dataAndCredentials, headers=Login.headers, verify=False)
             # Print the HTML source of the page after login
             print response.text
 
@@ -98,60 +98,58 @@ class Login(object):
             raise
 
         else:
-            # save cookies to local storage to maintain the seesion
-            # (connection)
+            # save cookies to local storage to maintain the seesion (connection)
             session.cookies.save(ignore_discard=True, ignore_expires=True)
 
-        def SessionMaintain(self, home_url):
-            """
-            Maintain the previous session with cookies
-            """
-            print 'Session recovering...'
+    def SessionMaintain(self, home_url, session):
+        """
+        Maintain the previous session with cookies
+        """
+        print 'Session recovering...'
 
-            try:
-                # Try to load the cookies stocked in a local file
-                print 'Loading cookies...'
-                session.cookies.load(
-                    filename=Login.filePath, ignore_discard=True, ignore_expires=True)
-                print 'Cookie loaded with success, waiting for the response'
-                # When cookies loaded, go to the site after login
-                response = session.get(Login.home_url, verify=False)
-                # Print the page's HTML
-                print response.text
-                # if rep.cookies.get_dict():
-                #     self.cookies.update(r.cookies)
+        try:
+            # Try to load the cookies stocked in a local file
+            print 'Loading cookies...'
+            session.cookies.load(
+                filename=Login.filePath, ignore_discard=True, ignore_expires=True)
+            print 'Cookie loaded with success, waiting for the response'
+            # When cookies loaded, go to the site after login
+            response = session.get(self.home_url, verify=False)
+            # Print the page's HTML
+            print response.text
+            # if rep.cookies.get_dict():
+            #     self.cookies.update(r.cookies)
 
-            # Error check when loging
-            except requests.RequestException as e:
-                print "There was an ambiguous exception that occurred while handling your request."
-                print "Error details : " + e
+        # Error check when loging
+        except requests.RequestException as e:
+            print "There was an ambiguous exception that occurred while handling your request."
+            print "Error details : " + e
 
-            except requests.ConnectionError as e:
-                print "A Connection error occurred."
-                print "Error details : " + e
+        except requests.ConnectionError as e:
+            print "A Connection error occurred."
+            print "Error details : " + e
 
-            except requests.HTTPError as e:
-                print "An HTTP error occurred."
-                print "Error details : " + e
+        except requests.HTTPError as e:
+            print "An HTTP error occurred."
+            print "Error details : " + e
 
-            except requests.URLRequired as e:
-                print "A valid URL is required to make a request."
-                print "Error details : " + e
+        except requests.URLRequired as e:
+            print "A valid URL is required to make a request."
+            print "Error details : " + e
 
-            except requests.TooManyRedirects as e:
-                print "Too many redirects."
-                print "Error details : " + e
+        except requests.TooManyRedirects as e:
+            print "Too many redirects."
+            print "Error details : " + e
 
-            except Exception as e:
-                raise
-            else:
-                # save cookies to local storage to maintain the seesion
-                # (connection)
-                session.cookies.save(ignore_discard=True, ignore_expires=True)
+        except Exception as e:
+            raise
+        else:
+            # save cookies to local storage to maintain the seesion (connection)
+            session.cookies.save(ignore_discard=True, ignore_expires=True)
 
         def Getcsrf(self):
             """
-            Get the _csrf parameter (this function is not necessary for the moment)
+            Get the csrf parameter (this function is not necessary for the moment)
             """
             response = session.get(self.login_url, headers=Login.headers)
             html = response.text
@@ -159,15 +157,15 @@ class Login(object):
             get_csrf_pattern = re.compile(
                 r'< the match pattern for the regular expression search >')
             '''
-               find all the information corresponding to the pattern, and return a list.
+               Find all the information corresponding to the pattern, and return a list.
                Normally, there'll be just one element in the list. So take the first element
             '''
             _csrf = re.findall(get_csrf_pattern, html)[0]
-            # returloginn the element extracted previously
+            # return the element extracted previously
             return _csrf
 
 
-class Extract(object):
+class ExtractLogs(object):
 
     def __init__(self):
         pass
@@ -178,5 +176,4 @@ class WriteExcel(object):
     def __init__(self):
         pass
 
-# print os.path.dirname(os.path.abspath(__file__))
 # print os.getcwd()
